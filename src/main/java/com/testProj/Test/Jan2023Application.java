c6a76cfa7ccb28c7933ff088d3be171b4fc5f53d
noobatlinux <nsaiteja03041998@gmail.com>
2023-01-02 21:53:54 -0600

    Project initialization

---
+++ src/main/java/com/testProj/Test/Jan2023Application.java
@@ -0,0 +1,13 @@
+package com.testProj.Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jan2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jan2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/testProj/Test/Jan2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testProj.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jan2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
